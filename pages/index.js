bf6d9c1b7d1ae59433730ff3bdf41fbcf76d2af5
mauro bono <bono.mauro@gmail.com>
2021-12-05 10:24:40 +0100

    [first] first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Nextjs Starter Theme</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Personal <a href='https://nextjs.org'>Next.js!</a> Starter Theme
+        </h1>
+      </main>
+    </div>
+  );
+}
